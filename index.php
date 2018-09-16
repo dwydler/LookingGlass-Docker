9a51792e72dc608a7433f6bd1842f893fdeec5c3
StadtBadWurzach <43094829+StadtBadWurzach@users.noreply.github.com>
2018-09-16 13:28:35 +0200

    add support for ipv4/ipv6 domains

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if (file_exists('LookingGlass/Config.php')) {
 							Network information
 							<?php
 							if ( (!empty($siteUrlv4)) &&  (!empty($siteUrlv6)) ) {
-								echo "( <a href=\"".$siteUrl."\">DualStack</a> | ";
+								echo "( <a href=\"".$siteUrl."\">DualStack</a> | <a href=\"".$siteUrlv4."\">IPv4</a> | <a href=\"".$siteUrlv6."\">IPv6</a> ";
 							}
 							?>
 							
