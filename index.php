48ed7a555573bcf130b6accb14ca29eb39e57d7a
StadtBadWurzach <43094829+StadtBadWurzach@users.noreply.github.com>
2018-09-18 10:15:38 +0200

    integrate Browser-Update.org

--- index.php
+++ index.php
@@ -31,10 +31,18 @@ if (isset($_GET["lang"])) {
 		
 		<title><?php echo $siteName; ?> - Looking Glass</title>
 
-		<!-- IE6-8 support of HTML elements -->
-		<!--[if lt IE 9]>
-		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
+		<script> 
+		var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,unsupported:true,api:2018.09 }; 
+		
+		function $buo_f(){ 
+			var e = document.createElement("script"); 
+			e.src = "//browser-update.org/update.min.js"; 
+			document.body.appendChild(e);
+		};
+
+		try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
+		catch(e){window.attachEvent("onload", $buo_f)}
+		</script>
 
 		<!-- Styles -->
 		<link href="assets/css/<?php echo $theme; ?>" rel="stylesheet" />
