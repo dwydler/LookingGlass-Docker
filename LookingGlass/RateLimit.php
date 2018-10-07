395ae4456ba106ce492d9b86e415a3836add9bc4
Whitemoon <daniel@wydler.eu>
2018-10-07 23:11:47 +0200

    create function for translate error code in error message

--- LookingGlass/RateLimit.php
+++ LookingGlass/RateLimit.php
@@ -59,7 +59,9 @@ class RateLimit
             // create new record
             $q = $dbh->prepare('INSERT INTO RateLimit (ip, hits, accessed) VALUES (?, ?, ?)');
             $q->execute(array($_SERVER['REMOTE_ADDR'], 1, $time));
-            return true;
+
+			// check error code of exec
+			$this->ErrorCheck($q);
         }
 
         // typecast SQLite results
@@ -71,17 +73,23 @@ class RateLimit
             if ($hits >= $limit) {
                 $reset = (int) (($accessed - $time) / 60);
                 if ($reset <= 1) {
-                    exit('Rate limit exceeded. Try again in: 1 minute');
+                    exit('Rate limit exceeded. Try again in 1 minute.');
                 }
-                exit('Rate limit exceeded. Try again in: ' . $reset . ' minutes');
+                exit('Rate limit exceeded. Try again in '.$reset.' minutes.');
             }
             // update hits
             $q = $dbh->prepare('UPDATE RateLimit SET hits = ? WHERE ip = ?');
             $q->execute(array(($hits + 1), $_SERVER['REMOTE_ADDR']));
+			
+			// check error code of exec
+			$this->ErrorCheck($q);
         } else {
             // reset hits + accessed time
             $q = $dbh->prepare('UPDATE RateLimit SET hits = ?, accessed = ? WHERE ip = ?');
             $q->execute(array(1, time(), $_SERVER['REMOTE_ADDR']));
+			
+			// check error code of exec
+			$this->ErrorCheck($q);
         }
 		
 		// close database connection
@@ -89,4 +97,72 @@ class RateLimit
 		
         return true;
     }
+	
+	/**
+     * Check errror code of sql execution
+     *
+     * @param  array $error
+     *   Array of errorCode und ErrorInfo
+     * @return boolean
+     *   True on success
+     */
+	private function ErrorCheck ($error) {
+
+		$errors = $error->errorInfo();
+		
+		switch ($errors[1]) {
+			case '1':
+				exit('Error: SQL error or missing database.');
+			case '2':
+				exit('Error: An internal logic error in SQLite.');
+			case '3':
+				exit('Error: Access permission denied.');
+			case '4':
+				exit('Error: Callback routine requested an abort.');
+			case '5':
+				exit('Error: The database file is locked.');
+			case '6':
+				exit('Error: A table in the database is locked.');
+			case '7':
+				exit('Error: A malloc() failed.');
+			case '8':
+				exit('Error: Attempt to write a readonly database.');
+			case '9':
+				exit('Error: Operation terminated by sqlite_interrupt().');
+			case '10':
+				exit('Error: Some kind of disk I/O error occurred.');
+			case '11':
+				exit('Error: The database disk image is malformed.');
+			case '12':
+				exit('Error: (Internal Only) Table or record not found.');
+			case '13':
+				exit('Error: Insertion failed because database is full.');
+			case '14':
+				exit('Error: Unable to open the database file.');
+			case '15':
+				exit('Error: Database lock protocol error.');
+			case '16':
+				exit('Error: (Internal Only) Database table is empty.');
+			case '17':
+				exit('Error: The database schema changed.');
+			case '18':
+				exit('Error: Too much data for one row of a table.');
+			case '19':
+				exit('Error: Abort due to contraint violation.');
+			case '20':
+				exit('Error: Data type mismatch.');
+			case '21':
+				exit('Error: Library used incorrectly.');
+			case '22':
+				exit('Error: Uses OS features not supported on host.');
+			case '23':
+				exit('Error: Authorization denied.');
+			case '100':
+				exit('Error: sqlite_step() has another row ready.');
+			case '103':
+				exit('Error: sqlite_step() has finished executing.');
+			default:
+				return true;
+		}
+	}
 }
