3039d11853b62639d3ca62a637599a7982e9d61e
wd <noemailaddress@forgit.de>
2023-06-04 13:50:03 +0200

    Removed the tool Browser-Update.org

--- index.php
+++ index.php
@@ -93,16 +93,6 @@ else {
                 <title><?php echo $siteName; ?></title>
 
                 <script>
-                // Browser-Update.org
-                var $buoop = {required:{e:-4,f:-3,o:-3,s:-1,c:-3},insecure:true,api:2023.06 };
-                function $buo_f(){
-                var e = document.createElement("script");
-                e.src = "//browser-update.org/update.min.js";
-                document.body.appendChild(e);
-                };
-                try {document.addEventListener("DOMContentLoaded", $buo_f,false)}
-                catch(e){window.attachEvent("onload", $buo_f)}
-
                 // Translation for JavaScript -->
                 RunTest = "<?php echo _("Run Test"); ?>"
                 Loading = "<?php echo _("Loading"); ?>"
