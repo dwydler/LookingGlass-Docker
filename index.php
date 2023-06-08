d8528316b16ea57bb767a0f48ed977d79568b843
wd <noemailaddress@forgit.de>
2023-06-08 15:55:11 +0200

    Fixed hash value of min.js

--- index.php
+++ index.php
@@ -298,7 +298,7 @@ if ( !isset ($_SESSION["theme"])) {
                 </div>
 
                 <script src="assets/js/jquery-3.7.0.min.js" integrity="sha384-NXgwF8Kv9SSAr+jemKKcbvQsz+teULH/a5UNJvZc6kP47hZgl62M1vGnw6gHQhb1" crossorigin="anonymous"></script>
-                <script src="./assets/js/LookingGlass.min.js" integrity="sha384-hQ7ko7+6ZTZD/rbIFXjrpVgqS5eK9Ou3MmCevnuWF6VEQZbYB9BkAnoBw5u8rPvT" crossorigin="anonymous"></script>
+                <script src="./assets/js/LookingGlass.min.js" integrity="sha384-CrU6npD/8yC+LPOJp5dyyYYK7CISjfADVfRHpaOL9of+cRe766unHiTsdQRN7nm9" crossorigin="anonymous"></script>
                 <script src="assets/js/XMLHttpRequest.min.js" integrity="sha384-4VZLxIdUn1yFnVIpiKDpf6aLhTZKitRINfJd0cyDjhM9c0fh+GzljUdTy38VXCvP" crossorigin="anonymous"></script>
         </body>
 </html>
