e3bf0bb29beb780f1be5b286041e909c56d6d2f8
Daniel Wydler <daniel@wydler.eu>
2024-01-07 14:18:27 +0100

    Added script to clone the git repo

---
+++ docker/misc/05-provisioning-lookingclass-files.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+# Create folder for docker containers
+mkdir -p /opt/containers/
+
+# Clone repository to local folder
+git clone https://codeberg.org/wd/LookingGlass-Docker.git /opt/containers/lookingglass/
+
+# Display content of the folder
+ls -lisa /opt/containers/lookingglass/
