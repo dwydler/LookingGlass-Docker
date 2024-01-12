9ffb0a8137bdd7dae3bf143d3c44999710f35f98
Daniel Wydler <daniel.wydler@mailbox.org>
2024-01-12 18:22:37 +0000

    Append docker build function

--- docker/misc/10-docker-hub-upload-containers.sh
+++ docker/misc/10-docker-hub-upload-containers.sh
@@ -12,20 +12,33 @@ fi
 # Enter the tag for the container
 read -e -p "Enter the tag for the container: " CONTAINERTAG
 
-# Array with the name of containers
-aContainerNames=(wydler/lookingglass-web wydler/lookingglass-php wydler/lookingglass-iperf)
+#
+docker buildx create --name lookingglass
 
-# 
-for i in "${aContainerNames[@]}"; do
-	echo "Container '$i' wird bearbeitet:";
-	
-	echo "Get the container ID of the named container.";
-	CONTAINERID=$(docker ps | grep "wydler/lookingglass-web" | awk {'print $1'} )
-	
-	echo "Output docker commands:";
-	echo "docker commit $CONTAINERID $i:$CONTAINERTAG";
-	echo "docker push $i:$CONTAINERTAG";
+#
+docker buildx use lookingglass
 
-	echo
-done
+
+#
+cd /opt/containers/LookingGlass-Docker/docker/nginx
+docker buildx build \
+       	--platform linux/arm64,linux/amd64 \
+       	-t wydler/lookingglass-web:$CONTAINERTAG \
+       	. \
+	--push
+
+cd /opt/containers/LookingGlass-Docker/docker/php-fpm
+docker buildx build \
+       	--platform linux/arm64,linux/amd64 \
+       	-t wydler/lookingglass-php:$CONTAINERTAG \
+       	. \
+       	--push
+
+cd /opt/containers/LookingGlass-Docker/docker/ipref3
+docker buildx build \
+       	--platform linux/arm64,linux/amd64 \
+       	-t wydler/lookingglass-iperf:$CONTAINERTAG \
+       	. \
+       	--push
+
 
