11d672602f4856ab85526c4792122052552f28ff
Blackmoon <dwydler@users.noreply.github.com>
2024-04-28 12:36:57 +0200

    Replaced Url for the Code Repository

--- docker/misc/01-provisioning-lookingclass-files.sh
+++ docker/misc/01-provisioning-lookingclass-files.sh
@@ -4,7 +4,7 @@
 mkdir -p /opt/containers/
 
 # Clone repository to local folder
-git clone https://codeberg.org/wd/LookingGlass-Docker.git /opt/containers/lookingglass/
+git clone https://github.com/dwydler/LookingGlass-Docker.git /opt/containers/lookingglass/
 
 # Display content of the folder
 ls -lisa /opt/containers/lookingglass/
